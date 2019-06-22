bbb3598334435390c46e18dada4b7eaf3dbf8267
Monsur <monsur1911@gmail.com>
2019-06-22 10:38:46 +0600

    adding post to healthsuggest

--- PROJECT CODE/HealthyNSU/healthsuggest/views.py
+++ PROJECT CODE/HealthyNSU/healthsuggest/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts = [
+    {
+        'author' : 'Monsur',
+        'title' : 'FirstPost',
+        'content' : 'First post content',
+        'date_posted' : 'June 22, 2019'
+    },
+    {
+        'author' : 'Hillas',
+        'title' : 'SecondPost',
+        'content' : 'Second post content',
+        'date_posted' : 'June 23, 2019'
+    }
+        ]
 
 def home(request):
-    return render(request, 'healthsuggest/healthsuggest.html')
+    context ={
+    'posts' : posts
+    }
+    return render(request, 'healthsuggest/healthsuggest.html',context)
+
+
 
 # Create your views here.
