eebc1367ada85d2451a622a8653a1324a59f2978
Monsur <monsur1911@gmail.com>
2019-06-29 18:47:27 +0600

    user app created for registration

--- Project Code/HealthyNSU/HealthyNSU/settings.py
+++ Project Code/HealthyNSU/HealthyNSU/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UserConfig',
     'home.apps.HomeConfig',
     'healthsuggest.apps.HealthsuggestConfig',
     'django.contrib.admin',
