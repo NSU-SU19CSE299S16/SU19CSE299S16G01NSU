69a8e3bc3be34f7349b285a0a30e4aa8f8059b5e
Monsur <monsur1911@gmail.com>
2019-06-29 19:07:32 +0600

    user registration minimul page added #4

--- Project Code/HealthyNSU/HealthyNSU/settings.py
+++ Project Code/HealthyNSU/HealthyNSU/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
     'home.apps.HomeConfig',
     'healthsuggest.apps.HealthsuggestConfig',
     'django.contrib.admin',
