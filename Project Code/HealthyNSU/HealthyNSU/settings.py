1b538b81bf47b349d0a93034f3e83f9a0a1a44fe
Monsur <monsur1911@gmail.com>
2019-06-29 21:24:06 +0600

    crispy forms added to register #4

--- Project Code/HealthyNSU/HealthyNSU/settings.py
+++ Project Code/HealthyNSU/HealthyNSU/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
     'home.apps.HomeConfig',
     'healthsuggest.apps.HealthsuggestConfig',
     'django.contrib.admin',
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
