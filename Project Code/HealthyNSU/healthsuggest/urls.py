5ad69bec4fed7e90f59b72c7b8d154ab3981e3d2
Monsur <monsur1911@gmail.com>
2019-07-10 00:50:40 +0600

    Issue #12 : Individual user post view has been added to the website

--- Project Code/HealthyNSU/healthsuggest/urls.py
+++ Project Code/HealthyNSU/healthsuggest/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListViewS,PostDetailViewS,PostCreateViewS,PostUpdateViewS,PostDeleteViewS
+from .views import PostListViewS,PostDetailViewS,PostCreateViewS,PostUpdateViewS,PostDeleteViewS,UserPostListViewS
 from .views import PostListViewA,PostDetailViewA,PostCreateViewA,PostUpdateViewA
 
 
 
 urlpatterns = [
     path('suggest/',PostListViewS.as_view() , name='HealthyNSU-healthsuggest'),
+    path('suggest/user/<str:username>', UserPostListViewS.as_view() , name='user-posts'),
     path('suggest/post/<int:pk>',PostDetailViewS.as_view() , name='post-detailsug'),
     path('suggest/post/new/',PostCreateViewS.as_view() , name='post-create'),
     path('suggest/post/<int:pk>/update/',PostUpdateViewS.as_view() , name='post-update'),
--- Project Code/HealthyNSU/healthsuggest/views.py
+++ Project Code/HealthyNSU/healthsuggest/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import (ListView,
                                 DetailView,
                                 CreateView,
@@ -23,6 +24,21 @@ class PostListViewS(LoginRequiredMixin, ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 3
+
+
+class UserPostListViewS(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'healthsuggest/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_query_set(self):
+        user = get_object_or_404(User,username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date-posted')
+
+
+
+
 
 class PostDetailViewS(LoginRequiredMixin, DetailView):
     model = Post
